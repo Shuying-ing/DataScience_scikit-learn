bdd3d8ae9c1277a68cb039c66ddc99899b473b91
Shuying-ing <QSY-5208@sjtu.edu.cn>
2023-07-22 18:33:32 +0800

    Add LinearModels-RANSAC/Theil_Sen/Quantile

--- Chap01 Supervised learning/Models/01-LinearModels.py
+++ Chap01 Supervised learning/Models/01-LinearModels.py
@@ -449,7 +449,7 @@ def Logistic_Regression(C, penalty):
     """1.1.11. Logistic regression
 
     Estimate the probability of an event occurring
-    
+
     Parameters
     ----------
     C : float
@@ -515,8 +515,8 @@ def Logistic_Regression(C, penalty):
 def GeneralizedLinearModels():
     """1.1.12. Generalized Linear Models
 
-    
-    
+    An extension of linear models
+
     Notes
     -----
     GLM extend linear models:
@@ -540,6 +540,206 @@ def GeneralizedLinearModels():
     print("R2 score: %.2f" % r2_score_lasso)
 
 
+def RANdom_SAmple_Consensus():
+    """1.1.16.2. RANSAC: RANdom SAmple Consensus
+
+    Fit a regression model in the presence of corrupt data: either outliers,
+        or error in the model.
+
+    Notes
+    -----
+    fits a model from random subsets of inliers from the complete data set
+    a non-deterministic algorithm producing only a reasonable result with a certain probability
+    be typically used for linear and non-linear regression problems
+    be especially popular in the field of photogrammetric computer vision
+    """
+
+    # 准备数据
+    n_samples = 1000
+    n_outliers = 50
+    X, y, coef = datasets.make_regression(
+        n_samples=n_samples,
+        n_features=1,
+        n_informative=1,
+        noise=10,
+        coef=True,
+        random_state=0,
+    )
+    # Add outlier data
+    np.random.seed(0)
+    X[:n_outliers] = 3 + 0.5 * np.random.normal(size=(n_outliers, 1))
+    y[:n_outliers] = -3 + 10 * np.random.normal(size=n_outliers)
+
+    # 拟合——普通模型（所有数据）
+    lr = linear_model.LinearRegression()
+    lr.fit(X, y)
+    # 拟合——ransac模型（抽取数据）
+    ransac = linear_model.RANSACRegressor()
+    ransac.fit(X, y)
+    inlier_mask = ransac.inlier_mask_  # 找出非异常点
+    outlier_mask = np.logical_not(inlier_mask)  # 取反
+
+    # 测试
+    line_X = np.arange(X.min(), X.max())[:, np.newaxis]  # newaxis-升维
+    line_y = lr.predict(line_X)
+    line_y_ransac = ransac.predict(line_X)
+    print("Estimated coefficients (true, linear regression, RANSAC):")
+    print(coef, lr.coef_, ransac.estimator_.coef_)
+    plt.scatter(
+        X[inlier_mask], y[inlier_mask], color="yellowgreen", marker=".", label="Inliers"
+    )
+    plt.scatter(
+        X[outlier_mask], y[outlier_mask], color="gold", marker=".", label="Outliers"
+    )
+    plt.plot(line_X, line_y, color="navy", linewidth=2, label="Linear regressor")
+    plt.plot(
+        line_X,
+        line_y_ransac,
+        color="cornflowerblue",
+        linewidth=2,
+        label="RANSAC regressor",
+    )
+    plt.legend(loc="lower right")
+    plt.xlabel("Input")
+    plt.ylabel("Response")
+
+
+def TheilSen_Regression():
+    """1.1.16.3. Theil-Sen estimator: generalized-median-based estimator
+
+    Fit a regression model in the presence of multivariate outliers
+
+    Notes
+    -----
+    uses a generalization of the median in multiple dimensions
+    be robust to multivariate outliers
+    loses its robustness properties and becomes no better than an ordinary least squares in high dimension
+    In certain cases Theil-Sen performs better than RANSAC which is also a robust method.
+    """
+
+    # 准备数据
+    np.random.seed(0)
+    n_samples = 200
+    X = np.random.randn(n_samples)
+    w = 3.0
+    c = 2.0
+    noise = 0.1 * np.random.randn(n_samples)
+    y = w * X + c + noise
+    X1 = X
+    y1 = y
+
+    fig, axs = plt.subplots(1, 2, figsize=(10, 5))
+    estimators = [
+        ("OLS", linear_model.LinearRegression()),
+        ("Theil-Sen", linear_model.TheilSenRegressor(random_state=42)),
+        ("RANSAC", linear_model.RANSACRegressor(random_state=42)),
+    ]
+    colors = {"OLS": "turquoise", "Theil-Sen": "gold", "RANSAC": "lightgreen"}
+
+    # 拟合 + 可视化
+    # 10% outliers [Outliers only in the y direction]
+    y[-20:] += -20 * X[-20:]
+    X = X[:, np.newaxis]
+    line_x = np.array([-3, 3])  # (2,)
+    axs[0].scatter(X, y, color="indigo", marker="x", s=40)
+    for name, estimator in estimators:
+        estimator.fit(X, y)
+        y_pred = estimator.predict(line_x.reshape(2, 1))
+        axs[0].plot(
+            line_x,
+            y_pred,
+            color=colors[name],
+            linewidth=2,
+            label="%s" % (name),
+        )
+    axs[0].legend(loc="upper left")
+    axs[0].set_title("Corrupt y")
+
+    # 10% outliers [Outliers only in the x direction]
+    X1[-20:] = 9.9
+    y1[-20:] += 22
+    X1 = X1[:, np.newaxis]
+    line_x = np.array([-3, 10])  # (2,)
+    axs[1].scatter(X1, y1, color="indigo", marker="x", s=40)
+    for name, estimator in estimators:
+        estimator.fit(X1, y1)
+        y_pred = estimator.predict(line_x.reshape(2, 1))
+        axs[1].plot(
+            line_x,
+            y_pred,
+            color=colors[name],
+            linewidth=2,
+            label="%s" % (name),
+        )
+    axs[1].legend(loc="upper left")
+    axs[1].set_title("Corrupt x")
+
+
+def Quantile_Regressor():
+    """1.1.17. Quantile Regression
+
+    Estimate the median or other quantiles of y conditional on X
+
+    Notes
+    -----
+    As the pinball loss is only linear in the residuals,
+        quantile regression is much more robust to outliers than squared error based estimation of the mean.
+    may be useful in predicting an interval instead of point prediction
+    """
+
+    # 准备数据
+    rng = np.random.RandomState(42)
+    x = np.linspace(start=0, stop=10, num=100)
+    X = x[:, np.newaxis]
+    y_true_mean = 10 + 0.5 * x
+    y_normal = y_true_mean + rng.normal(loc=0, scale=0.5 + 0.5 * x, size=x.shape[0])
+
+    # 拟合
+    quantiles = [0.05, 0.5, 0.95]
+    predictions = {}
+    out_bounds_predictions = np.zeros_like(
+        y_true_mean, dtype=np.bool_
+    )  # zeros_like-与给定数组具有相同形状和数据类型的零数组
+    for quantile in quantiles:
+        qr = linear_model.QuantileRegressor(quantile=quantile, alpha=0)
+        y_pred = qr.fit(X, y_normal).predict(X)
+        predictions[quantile] = y_pred
+
+        if quantile == min(quantiles):
+            out_bounds_predictions = np.logical_or(
+                out_bounds_predictions, y_pred >= y_normal
+            )  #  logical_or-在两个布尔数组之间执行逻辑或运算
+        elif quantile == max(quantiles):
+            out_bounds_predictions = np.logical_or(
+                out_bounds_predictions, y_pred <= y_normal
+            )
+
+    # 可视化
+    for quantile, y_pred in predictions.items():
+        plt.plot(X, y_pred, label=f"Quantile: {quantile}")
+
+    plt.scatter(
+        x[out_bounds_predictions],
+        y_normal[out_bounds_predictions],
+        color="black",
+        marker="+",
+        alpha=0.5,
+        label="Outside interval",
+    )
+    plt.scatter(
+        x[~out_bounds_predictions],
+        y_normal[~out_bounds_predictions],
+        color="black",
+        alpha=0.5,
+        label="Inside interval",
+    )
+
+    plt.legend()
+    plt.xlabel("x")
+    plt.ylabel("y")
+    _ = plt.title("Quantiles of heteroscedastic Normal distributed target")
+
+
 if __name__ == "__main__":
     # Ordinary_LeastSquares_LinearRegression()  # OLS
     # NonNegative_LeastSquares_LinearRegression()  # NNLS
@@ -553,5 +753,8 @@ if __name__ == "__main__":
     # BayesianRidge_Regression()  # BayesianRidge
     # Automatic_Relevance_Determination()  # ARD
     # Logistic_Regression(C=0.1, penalty="l2")  # Logistic / LogisticRegressionCV
-    GeneralizedLinearModels()  # GLM
+    # GeneralizedLinearModels()  # GLM
+    # RANdom_SAmple_Consensus()  # RANSAC
+    # TheilSen_Regression()  # Theil_Sen
+    Quantile_Regressor()  # Quantile
     plt.show()
